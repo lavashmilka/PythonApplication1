a7ef2f8fc8d47ed62896d663f6d0477dfa3afed9
opilane <opilane@HP-CZC2349HT9>
2025-03-20 09:34:38 +0200

    Add project files.

---
+++ PythonApplication1.py
@@ -0,0 +1,47 @@
+# from datetime import *
+# from calendar import *
+# from math import *
+# from time import strptime
+
+# #‹lesanne 1
+# tana=date.today() 
+# print(f"Tere! T‰na on {tana}")
+
+# # 27/12/2022
+# tana1 = tana.strftime("%d/%m/%Y")
+# print(tana1)
+
+# # December 27, 2022
+# tana2 = tana.strftime("%B %d, %Y")
+# print(tana2)
+
+# # 12/27/22
+# tana3 = tana.strftime("%m/%d/%y")
+# print(tana3)
+
+# # Dec-27-2022
+# tana4 = tana.strftime("%b-%d-%Y")
+# print(tana4)
+
+# p‰ev=tana.day
+# kuu=tana.month
+# aasta=tana.year
+
+# print(f"P‰ev on {p‰ev}, \nKuu on {kuu}, \nAasta on {aasta}")
+
+# paevad=monthrange(2025,2)[1]
+# onjaanud=paevad-p‰ev
+# print(f"kuu lıpuni on j‰‰nud {onjaanud} p‰evad")
+
+# paev1=monthrange(2025,2)[1]
+# paev2=monthrange(2025,3)[1]
+# paev3=monthrange(2025,4)[1]
+# paev4=monthrange(2025,5)[1]
+# paev5=monthrange(2025,6)[1]
+# paev6=monthrange(2025,7)[1]
+# paev7=monthrange(2025,8)[1]
+
+
+
+
+
